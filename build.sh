ac74cd3474551d629d1deb85c87a8e538e975acf
MD ARIFUL HASAN <77568259+mdahtotul@users.noreply.github.com>
2023-09-05 00:29:44 +0600

    pipenv added again

--- build.sh
+++ build.sh
@@ -1,3 +1,9 @@
-echo 'running migrations...'
+echo "Installing pipenv..."
+pip install pipenv
+
+echo "Installing packages.."
+pipenv install
+
+echo "Running migrations..."
 python3 manage.py collectstatic --no-input
 python3 manage.py migrate
